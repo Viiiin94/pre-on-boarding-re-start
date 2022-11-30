366cba561deb7ae2378cb1e3832a4e25df7fe259
BTSBRINGMEHERE <mynameisyyb@naver.com>
2022-11-30 16:40:05 +0900

    simple todolist markup #2
    simple todolist markup #2

--- client/src/Components/Todo/TodoList.tsx
+++ client/src/Components/Todo/TodoList.tsx
@@ -1,19 +1,58 @@
 import styled from "@emotion/styled"
 import React from "react"
+import { FiCircle, FiCheckCircle } from "react-icons/fi"
+import { CgAdd, CgRemove } from "react-icons/cg"
 
 const TodoList = () => {
 	return (
 		<>
-			<Container></Container>
+			<Container>
+				<TodoLists>
+					<div>
+						<FiCircle />
+					</div>
+					<span>투두리스트...</span>
+					<div></div>
+				</TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+				<TodoLists></TodoLists>
+			</Container>
 		</>
 	)
 }
 
-const Container = styled.main`
+const Container = styled.ol`
 	width: 355px;
 	height: 570px;
 	border: 1px solid black;
 	margin: 15px 15px 15px 0;
+	padding: 15px 15px;
+`
+
+const TodoLists = styled.li`
+	width: 325px;
+	height: 34px;
+	border: 1px solid black;
+	margin-top: 10px;
+	display: flex;
+	align-items: center;
+	div {
+		visibility: hidden;
+	}
+	&:hover {
+		div {
+			visibility: visible;
+		}
+	}
 `
 
 export default TodoList
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Provider } from "react-redux"
 import App from "./App"
 import "./index.css"
 
-const queryClient = new QueryClient()
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<QueryClientProvider client={queryClient}>
+	<>
 		<App />
-	</QueryClientProvider>
+	</>
 )
